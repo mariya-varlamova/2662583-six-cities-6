072b9033c0f40073d13f7a24024fa1a3fc2c9f5c
mariya-varlamova <mariya.varlamova.04@mail.ru>
2026-03-06 09:27:17 +0700

    Создание компонентов для страниц

---
+++ src/components/app/App.tsx
@@ -0,0 +1,13 @@
+import MainPage from '../pages/main-page/main-page';
+
+type AppProps = {
+  offersCount: number;
+}
+
+function App({ offersCount }: AppProps): JSX.Element{
+  return (
+    <MainPage offersCount={offersCount}/>
+  );
+}
+
+export default App;
